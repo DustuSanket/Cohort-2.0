ba4c28cd5302861f8d386d9f1b0ab8e9c3cdc4da
DustuSanket <sanketroy290@gmail.com>
2026-03-13 18:00:19 +0530

    now tabs done will move to the functioning

--- Dom-Project-1/script.js
+++ Dom-Project-1/script.js
@@ -1,1 +1,14 @@
 // https://in.pinterest.com/pin/71987294040806481/
+
+const allElems = document.querySelectorAll(".elems");
+const returnBtn = document.querySelectorAll(".return");
+allElems.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    document.querySelectorAll(".tabs")[elem.id].style.display = "block";
+  });
+});
+returnBtn.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    document.querySelectorAll(".tabs")[elem.id].style.display = "none";
+  });
+});
