f3e9e2ad11d38c7dc625b83b81af0ab6618eb876
DustuSanket <sanketroy290@gmail.com>
2026-04-21 18:54:56 +0530

    tasks are getting ready (coming back on tracks)

--- Dom-Project-1/script.js
+++ Dom-Project-1/script.js
@@ -1,14 +1,25 @@
 // https://in.pinterest.com/pin/71987294040806481/
 
-const allElems = document.querySelectorAll(".elems");
-const returnBtn = document.querySelectorAll(".return");
-allElems.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    document.querySelectorAll(".tabs")[elem.id].style.display = "block";
+function openContainer() {
+  const allElems = document.querySelectorAll(".elems");
+  const returnBtn = document.querySelectorAll(".return");
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      document.querySelectorAll(".tabs")[elem.id].style.display = "block";
+    });
   });
-});
-returnBtn.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    document.querySelectorAll(".tabs")[elem.id].style.display = "none";
+  returnBtn.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      document.querySelectorAll(".tabs")[elem.id].style.display = "none";
+    });
   });
+}
+openContainer();
+
+let taskForm = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form input");
+let taskDetails = document.querySelector(".addTask form textarea");
+
+taskForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 });
