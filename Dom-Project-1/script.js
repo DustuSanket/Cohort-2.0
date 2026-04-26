7cd0f55292cfa488fdc626e3dc86583606424b65
DustuSanket <sanketroy290@gmail.com>
2026-04-26 17:04:35 +0530

    local storage

--- Dom-Project-1/script.js
+++ Dom-Project-1/script.js
@@ -17,35 +17,30 @@ function openContainer() {
 openContainer();
 
 function renderTask() {
+  localStorage.setItem("currentTasks", JSON.stringify(currentTasks));
+
   let allTasks = document.querySelector(".allTasks");
   let sum = "";
 
-  currentTasks.forEach(function (e) {
+  currentTasks.forEach(function (e, idx) {
     sum += `<div class="tasks">
               <h5>${e.taskName}</h5>
-              <button>Mark as Done <i class="ri-check-line"></i></button>
+              <div class="task-btns">
+              <button id=${idx} class="dlt-btn"><i class="ri-delete-bin-5-line"></i></button>
+              <button id=${idx} class="markDone">Mark as Done <i class="ri-check-line"></i></button></div>
             </div>`;
   });
 
   allTasks.innerHTML = sum;
 }
-
-let currentTasks = [
-  {
-    taskName: "Hagne jao",
-    details: "zada se zada",
-  },
-  {
-    taskName: "Khana Khao",
-    details: "Roti chicken",
-  },
-  {
-    taskName: "Video dekho",
-    details: "Sheyrians ka",
-  },
-];
-
 renderTask();
+
+var currentTasks = [];
+if (localStorage.getItem("currentTasks")) {
+  currentTasks = JSON.parse(localStorage.getItem("currentTasks"));
+} else {
+  console.log("There are no tasks yet!");
+}
 
 let taskForm = document.querySelector(".addTask form");
 let taskInput = document.querySelector(".addTask form input");
@@ -54,7 +49,18 @@ let taskDetails = document.querySelector(".addTask form textarea");
 taskForm.addEventListener("submit", function (e) {
   e.preventDefault();
   currentTasks.push({ taskName: taskInput.value, details: taskDetails.value });
+
   taskInput.value = "";
   taskDetails.value = "";
   renderTask();
+});
+
+let dltBtn = document.querySelectorAll(".dlt-btn");
+
+dltBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    console.log(btn.id);
+    currentTasks.splice(btn.id, 1);
+    renderTask();
+  });
 });
