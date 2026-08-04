cb8cef129886323938b91def73d4cc0eca4ea4cd
DustuSanket <sanketroy290@gmail.com>
2026-08-04 10:27:17 +0530

    added post controller

--- Learning-Full-Stack/insta-clone/routes/post.route.js
+++ Learning-Full-Stack/insta-clone/routes/post.route.js
@@ -1,27 +1,8 @@
 const express = require("express");
-const multer = require("multer");
-const ImageKit = require("@imagekit/nodejs");
-const { toFile } = require("@imagekit/nodejs");
-
-const upload = multer({ storage: multer.memoryStorage() });
-const imageKit = new ImageKit({
-  privatekey: process.env.IMAGEKIT_PRIVATE_KEY,
-});
+const { createPost } = require("../controllers/post.controller");
 
 const postRouter = express.Router();
 
-postRouter.post("/", upload.single("file"), async (req, res) => {
-  const file = await imageKit.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer), "file"),
-    fileName: "fileName",
-  });
-
-  res.send({
-    name: file.name,
-    url: file.url,
-    size: Number((file.size / (1024 * 1024)).toFixed(2)) + "MB",
-    Id: file.fileId,
-  });
-});
+postRouter.post("/", upload.single("file"), createPost);
 
 module.exports = postRouter;
